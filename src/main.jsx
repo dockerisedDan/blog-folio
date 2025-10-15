d55db1949057d5d077f4e8eaa08f252618b2d211
Daniel <daniel.crockett98@gmail.com>
2025-10-15 21:50:12 +1100

    Initial site setup. Install tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <div className='bg-zinc-800 min-h-screen'>
+            <h1 className='text-white text-center'>Daniels Blog-Folio</h1>
+        </div>
+    </StrictMode>,
 )
