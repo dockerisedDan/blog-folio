2696958290106f1b657d39c2e66490d6a4633d96
Daniel <daniel.crockett98@gmail.com>
2025-10-16 22:46:30 +1100

    add react routing and basic page routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+
 import HomePage from "./pages/HomePage";
+import PostsPage from "./pages/PostsPage"
+import AboutPage from "./pages/AboutPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import PageLayout from "./components/PageLayout"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HomePage />
+        <Router basename="/blog-folio">
+            <Routes>
+                <Route path="/" element={<PageLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="posts" element={<PostsPage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+        </Router>
     </StrictMode>,
 );
